0a1d2d359ed3747612e63dba071e92228bf1edf6
Robyn <Robyn796@mac.com>
2024-02-24 13:02:01 -0700

    update

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
 // index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
 
 
 /* ***********************
